e26cc44ed78badc10bd44c6152af7544d5ea19a1
prochatcbm <cbm@prochi.de>
2017-02-08 14:32:26 +0100

    weiter
    weiter

--- Dienstag/src/Tag3_java_awt.java
+++ Dienstag/src/Tag3_java_awt.java
@@ -5,10 +5,12 @@ import java.awt.event.*;
  * Created by Administrator on 08.02.2017.
  */
 public class Tag3_java_awt {
-    static Frame frm;
+    Frame frm;
+    Label lbl1;
+    int cnter;
 
     Tag3_java_awt() {
-        Build_Frame(2, 1, 300, 300);
+        Build_Frame(3, 3, 300, 300);
         Build_Buttons("OK");
         Build_Labels("Label1");
     }
@@ -28,16 +30,32 @@ public class Tag3_java_awt {
 
     void Build_Buttons(String btnText) {
         Button btn = new Button(btnText);
+        btn.addActionListener(new MeineAktion());
         frm.add(btn);
     }
 
     void Build_Labels(String lbl_Text) {
-        Label lbl = new Label(lbl_Text);
-        frm.add(lbl);
+        lbl1 = new Label(lbl_Text);
+        frm.add(lbl1);
     }
 
-}
+    void setTest() {
+        Label lbl = new Label("Hannes");
+        frm.add(lbl);
 
+    }
+
+
+    class MeineAktion implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String s = e.getActionCommand();
+            if (s.equals("OK")) {
+                lbl1.setText(++cnter+" mal geklickt");
+            }
+        }
+    }
+}
 
 class ProgEnde extends WindowAdapter {
     @Override
