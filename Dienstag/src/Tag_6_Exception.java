51c8a7ef2b39b039134f422c942f8494f76b781a
prochatcbm <cbm@prochi.de>
2017-02-13 15:46:58 +0100

    d
    d

--- Dienstag/src/Tag_6_Exception.java
+++ Dienstag/src/Tag_6_Exception.java
@@ -23,15 +23,24 @@ public class Tag_6_Exception extends JFrame {
                 buildTestDialog_1();
                 break;
 
+            case 2:
+                buildTestDialog2();
+                break;
+
             default:
                 break;
         }
+    }
+
+
+    public void buildTestDialog2() {
 
 
     }
 
+
     public void buildTestDialog_1() {
-        meinJDialog.setTitle("INT test");
+        meinJDialog.setTitle("TestDialog_1 - IF IS INT");
         meinJDialog.setLayout(new GridLayout(2, 1));
         meinJDialog.addWindowListener(new EndProg());
         meinJDialog.setSize(450, 300);
@@ -44,10 +53,11 @@ public class Tag_6_Exception extends JFrame {
 
 
     public void stringChangeCommaToDot() {
-        StringBuffer strB=new StringBuffer(text.getText());
-        int deziPos=strB.indexOf(",");
-        System.out.println(deziPos+"---"+text);
-        strB.setCharAt(deziPos,'.');
+        StringBuffer strB = new StringBuffer(text.getText());
+        int deziPos = strB.indexOf(",");
+        //System.out.println(deziPos);
+        strB.setCharAt(deziPos, '.');
+        text.setText(String.valueOf(strB));
     }
 
     public static void main(String[] args) {
@@ -56,15 +66,36 @@ public class Tag_6_Exception extends JFrame {
 
     class Feld1 extends KeyAdapter {
         @Override
+
         public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                System.exit(0);
+            }
+
+            if (e.getKeyCode() == KeyEvent.VK_0) {
+                stringChangeCommaToDot();
+            }
+
+
             if (e.getKeyCode() == 10) {
                 try {
-                    int z = Integer.parseInt(text.getText());
-                    lab.setText("" + (z * 2));
+
+                    int hans = (int) Double.parseDouble(text.getText());
+                    lab.setText("" + (hans * 2));
+
                 } catch (NumberFormatException g) {
-                    JOptionPane.showMessageDialog(text, "keine int-Zahl");
-                    text.setText("");
+                    try {
+                        int z = Integer.parseInt(text.getText());
+                        lab.setText("" + (z * 2));
+
+                    } catch (NumberFormatException h) {
+                        JOptionPane.showMessageDialog(text, "keine int-Zahl");
+                        text.setText("");
+                    }
+
                 }
+
+
             }
         }
     }
